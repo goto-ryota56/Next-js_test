09fd2d91fef575b698476b79b3171dcc8863e65a
joetsu-goto <gotou0600@gmail.com>
2022-10-25 16:12:08 +0900

    cssに全変更

--- components/logo.js
+++ components/logo.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "styles/logo.module.scss";
+import styles from "styles/logo.module.css";
 
 export default function Logo({ boxOn = false }) {
     return (
