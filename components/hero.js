36c0f12500b802b42f2fda0e896e85e8e57ec937
joetsu-goto <gotou0600@gmail.com>
2022-10-25 15:34:38 +0900

    sass インストール　拡張子変更　import文変更

--- components/hero.js
+++ components/hero.js
@@ -1,14 +1,14 @@
+import styles from "styles/hero.module.scss";
+
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
-        <>
-            <main>
-                <div>
-                    <h1>{title}</h1>
-                    <p>{subtitle}</p>
-                    {imageOn && <figure>[画像]</figure>}
-                </div>
-            </main>
-        </>
+        <div>
+            <div className={styles.text}>
+                <h1 className={styles.title}>{title}</h1>
+                <p className={styles.subtitle}>{subtitle}</p>
+            </div>
+            {imageOn && <figure>[画像]</figure>}
+        </div>
     );
 }
 
