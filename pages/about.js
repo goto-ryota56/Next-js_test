f83598fa600ddf4b94c4f75f1a0ab3922436234a
joetsu-goto <gotou0600@gmail.com>
2022-11-07 16:43:13 +0900

    aboutページの画像反映。それに伴うスタイル追記

--- pages/about.js
+++ pages/about.js
@@ -3,12 +3,16 @@ import Container from "components/container";
 import PostBody from "components/post.body";
 import Contact from "components/contact";
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column";
+import Image from "next/image";
+import eyecatch from "images/about.jpg";
 
 export default function About() {
     return (
         <Container>
             <Hero title='About' subtitle='About development activities' />
-
+            <figure>
+                <Image src={eyecatch} alt='犬とパソコン' layout='responsive' pro sizes='(min-width: 1152px) 1152px, 100vw' priority placeholder='blur' />
+            </figure>
             <TwoColumn>
                 <TwoColumnMain>
                     <PostBody>
