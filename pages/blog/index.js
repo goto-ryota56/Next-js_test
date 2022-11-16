43dc74abaaf5961c1a654fb197e398e55e6e64a6
joetsu-goto <gotou0600@gmail.com>
2022-11-16 17:53:08 +0900

    アイキャッチ画像出力＋ない場合は代理出力

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,9 @@ import Hero from "components/hero";
 import Container from "components/container";
 import { getAllPosts } from "lib/api";
 import Posts from "components/posts";
+import { getPlaiceholder } from "plaiceholder";
+
+import { eyecatchLocal } from "lib/constants";
 
 export default function Blog({ posts }) {
     return (
@@ -14,8 +17,17 @@ export default function Blog({ posts }) {
         </Container>
     );
 }
+
 export async function getStaticProps() {
     const posts = await getAllPosts();
+
+    for (const post of posts) {
+        if (!post.hasOwnProperty("eyecatch")) {
+            post.eyecatch = eyecatchLocal;
+        }
+        const { base64 } = await getPlaiceholder(post.eyecatch.url);
+        post.eyecatch.blurDataURL = base64;
+    }
 
     return {
         props: {
